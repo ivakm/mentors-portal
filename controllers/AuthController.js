82fa91373140ee15dae07576930e763fb72fc19b
WIN-CRV48B2QV6T\user <dimashevchenko594@gmail.com>
2022-08-16 12:19:31 +0300

    changed default role for new user as a STUDENT

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -27,7 +27,7 @@ class AuthController {
             }
 
             const hashPassword = bcrypt.hashSync(password, 7);
-            const userRole = await Role.findOne({ value: "USER" });
+            const userRole = await Role.findOne({ value: "STUDENT" });
             const user = new User({ username, password: hashPassword, roles: [userRole.value] });
             await user.save();
             return res.json(user);
