c21ef78c7db8957f6aa2d9e1ee34e04a6156bac3
WIN-CRV48B2QV6T\user <dimashevchenko594@gmail.com>
2022-08-16 10:32:42 +0300

    added cors package

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './router.js';
+import cors from 'cors';
 
 
 const PORT = process.env.PORT || 5000;
 const DB_URL = `mongodb+srv://admin:admin@cluster0.vmzgk0y.mongodb.net/auth_roles?retryWrites=true&w=majority`
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use('/auth', router);
